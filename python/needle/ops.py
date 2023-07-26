69b52a411417dcbdfdd208e2fb082708d206ead8
chendixi05 <chendixi05@kuaishou.com>
2023-07-26 09:59:40 +0800

    sequential and logsumexp

--- python/needle/ops.py
+++ python/needle/ops.py
@@ -413,18 +413,36 @@ def relu(a):
 
 class LogSumExp(TensorOp):
     def __init__(self, axes: Optional[tuple] = None):
+        # Tuple of axes to sum and take the maximum element over
         self.axes = axes
 
     def compute(self, Z):
         ### BEGIN YOUR SOLUTION
-        raise NotImplementedError()
+        # maxz 也按照维度来
+        maxZ = array_api.max(Z, axis=self.axes, keepdims=True)
+        expSum = array_api.sum(array_api.exp(Z - maxZ), axis=self.axes)
+        return array_api.log(expSum) + array_api.max(Z, axis=self.axes, keepdims=False)
         ### END YOUR SOLUTION
 
     def gradient(self, out_grad, node):
         ### BEGIN YOUR SOLUTION
-        raise NotImplementedError()
-        ### END YOUR SOLUTION
+        z = node.inputs[0]
+        input_shape = z.shape
+        tmp_reshape = list(input_shape)
 
+        if self.axes is None:
+            return out_grad * exp(z - node)
+        elif isinstance(self.axes, tuple):
+            for summed_axe in self.axes:
+                tmp_reshape[summed_axe] = 1
+        elif isinstance(self.axes, int):
+            tmp_reshape[self.axes] = 1
+        
+        node_new = reshape(node, tuple(tmp_reshape))
+        grad_new = reshape(out_grad, tuple(tmp_reshape))
+
+        return grad_new * exp(z - node_new)
+        ### END YOUR SOLUTION
 
 def logsumexp(a, axes=None):
     return LogSumExp(axes=axes)(a)
